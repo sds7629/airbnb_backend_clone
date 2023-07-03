6df31be7cd0ab0e70d365da94a5215f8887542cb
sds7629 <sds7629@naver.com>
2023-07-04 00:44:38 +0900

    data수정

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.see_all_room),
-    path('<int:room_id>', views.see_one_room),
+    path("", views.see_all_room),
+    path("<int:room_id>", views.see_one_room),
 ]
